de8a7445647ab6872682eca268e791fc8ae25bfc
Lucas Montenegro <lucasmontenegro475@gmail.com>
2024-04-24 20:04:24 -0300

    :sparkles: feat: Created an infinite horizontal scroll animation

--- src/components/slide/Slide.tsx
+++ src/components/slide/Slide.tsx
@@ -5,29 +5,55 @@ export default function Slide() {
   const width = 50;
 
   return (
-    <div className="bg-[#1A1A1A] border-y-2 border-[#3C3C3C] h-60 p-10 flex gap-3 ">
-      <Icon name="javascript" width={width} className={className} />
-      <Icon name="react" width={width} color="#333333" className={className} />
-      <Icon name="typescript" width={width} className={className} />
-      <Icon name="vue" width={width} className={className} />
-      <Icon name="next" width={width} className={className} />
-      <Icon name="html" width={width} className={className} />
-      <Icon name="css" width={width} className={className} />
-      <Icon name="node" width={width} className={className} />
-      <Icon name="docker" width={width} className={className} />
-      <Icon name="svelte" width={width} className={className} />
-      <Icon name="figma" width={width} className={className} />
-      <Icon name="postgresql" width={width} className={className} />
-      <Icon name="npm" width={width} className={className} />
-      <Icon name="mysql" width={width} className={className} />
-      <Icon name="firebase" width={width} className={className} />
-      <Icon name="mongodb" width={width} className={className} />
-      <Icon name="knex" width={width} className={className} />
-      <Icon name="tailwind" width={width} className={className} />
-      <Icon name="git" width={width} className={className} />
-      <Icon name="webpack" width={width} className={className} />
-      <Icon name="yarn" width={width} className={className} />
-      <Icon name="jest" width={width} className={className} />
+    <div className="bg-[#1A1A1A] border-y-2 border-[#3C3C3C] p-10 flex overflow-hidden gap-6">
+      <div className="flex items-center gap-6 animate-infinite-scroll">
+        <Icon name="javascript" width={width} className={className} />
+        <Icon name="react" width={width} className={className} />
+        <Icon name="typescript" width={width} className={className} />
+        <Icon name="vue" width={width} className={className} />
+        <Icon name="next" width={width} className={className} />
+        <Icon name="html" width={width} className={className} />
+        <Icon name="css" width={width} className={className} />
+        <Icon name="node" width={width} className={className} />
+        <Icon name="docker" width={width} className={className} />
+        <Icon name="svelte" width={width} className={className} />
+        <Icon name="figma" width={width} className={className} />
+        <Icon name="postgresql" width={width} className={className} />
+        <Icon name="npm" width={width} className={className} />
+        <Icon name="mysql" width={width} className={className} />
+        <Icon name="firebase" width={width} className={className} />
+        <Icon name="mongodb" width={width} className={className} />
+        <Icon name="knex" width={width} className={className} />
+        <Icon name="tailwind" width={width} className={className} />
+        <Icon name="git" width={width} className={className} />
+        <Icon name="webpack" width={width} className={className} />
+        <Icon name="yarn" width={width} className={className} />
+        <Icon name="jest" width={width} className={className} />
+      </div>
+      <div className="flex items-center gap-6 animate-infinite-scroll">
+        <Icon name="javascript" width={width} className={className} />
+        <Icon name="react" width={width} className={className} />
+        <Icon name="typescript" width={width} className={className} />
+        <Icon name="vue" width={width} className={className} />
+        <Icon name="next" width={width} className={className} />
+        <Icon name="html" width={width} className={className} />
+        <Icon name="css" width={width} className={className} />
+        <Icon name="node" width={width} className={className} />
+        <Icon name="docker" width={width} className={className} />
+        <Icon name="svelte" width={width} className={className} />
+        <Icon name="figma" width={width} className={className} />
+        <Icon name="postgresql" width={width} className={className} />
+        <Icon name="npm" width={width} className={className} />
+        <Icon name="mysql" width={width} className={className} />
+        <Icon name="firebase" width={width} className={className} />
+        <Icon name="mongodb" width={width} className={className} />
+        <Icon name="knex" width={width} className={className} />
+        <Icon name="tailwind" width={width} className={className} />
+        <Icon name="git" width={width} className={className} />
+        <Icon name="webpack" width={width} className={className} />
+        <Icon name="yarn" width={width} className={className} />
+        <Icon name="jest" width={width} className={className} />
+      </div>
     </div>
   );
 }
