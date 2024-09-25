628bc4541a06eda5f0dfdfc5672789a8e0261d4a
Lucas Montenegro <lucasmontenegro475@gmail.com>
2024-09-25 19:46:50 -0300

    :recycle: refactor: remove unused Analytics component from _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,3 @@
-import Analytics from "../components/common/analytics/Analytics";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -8,7 +7,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   );
