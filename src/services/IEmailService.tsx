baa78df8212770c17d68aa1248bb17632fed71b5
Lucas <lucasmontenegro475@gmail.com>
2025-11-22 17:01:51 -0300

    :fire: chore: remove unused files and components, including configuration, styles, and assets

--- src/services/IEmailService.tsx
+++
@@ -1,9 +0,0 @@
-export type EmailParams = {
-  from_name: string;
-  from_email: string;
-  message: string;
-};
-
-export default interface IEmailService {
-  send(params: EmailParams): Promise<{ result: boolean }>;
-}
